bf810f0d1cdfbc2e6f758005580e4091c705cfe2
Vladislav Holod <vladislav.holod@humans.net>
2024-05-12 17:25:03 +0200

    Crud user. Add model and controller

---
+++ src/main/java/by/nexer/travelassistant/model/entity/User.java
@@ -0,0 +1,36 @@
+package by.nexer.travelassistant.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
