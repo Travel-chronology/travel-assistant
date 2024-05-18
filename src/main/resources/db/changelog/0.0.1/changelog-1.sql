4ebd97f560e1adad085faa3421b4a01e9e850270
Vladislav Holod <vladislav.holod@humans.net>
2024-05-18 17:41:16 +0200

    Add OpenApi

--- src/main/resources/db/changelog/0.0.1/changelog-1.sql
+++ src/main/resources/db/changelog/0.0.1/changelog-1.sql
@@ -2,16 +2,16 @@
 
 --changeset UladzislauKholad:1
 --comment first migration
-CREATE TABLE public.user(
+CREATE TABLE public.users(
 	id serial PRIMARY KEY,
-	username varchar(255) not null unique,
+	user_name varchar(255) not null unique,
 	first_name varchar(255),
 	last_name varchar(255),
 	email varchar(255),
 	password varchar(255) not null
 );
 
-insert into public.user (username, first_name, last_name, email, password)
+insert into public.users (user_name, first_name, last_name, email, password)
 values ('nexer', 'Uladzislau','kholad', '123@mail.ru','123'),
        ('razdva','Stepan','Ivanov', 'test@mail.ru','123123');
 --rollback truncate table customer;
